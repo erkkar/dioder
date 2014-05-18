a731047e8760c84331ab60a97b8f0b7901f969ad
Erkka Rinne <erkkar@iki.fi>
2014-05-18 15:10:31 +0300

    Strobe mode

--- dioder.ino
+++ dioder.ino
@@ -1,20 +1,42 @@
 const int outR = 9;
 const int outG = 11;
 const int outB = 10;
+
+unsigned long period = 0;
+byte r, g, b;
+byte strobe = 0;
  
 void setup() {
   Serial.begin(9600); 
- 
-  analogWrite(outR, 0);
-  analogWrite(outG, 0);
-  analogWrite(outB, 0);
+  r = 0;
+  g = 0;
+  b = 0;
+  strobe = 0;
 }
  
-void loop() {  
-  
-  if (Serial.available() == 3) {
-    analogWrite(outR, Serial.read());
-    analogWrite(outG, Serial.read());
-    analogWrite(outB, Serial.read());
+void loop() {    
+  if (Serial.available() >= 4) {
+    r = Serial.read();
+    g = Serial.read();
+    b = Serial.read();
+    strobe = Serial.read();
   }
+  setColor(r, g, b);
+  // Strobe
+  if (strobe > 0) {
+    period = float(strobe) / 255 * 1000;
+    delay(period / 2);
+    black();
+    delay(period / 2);
+  }
+}
+
+void setColor(byte _r, byte _g, byte _b) {
+  analogWrite(outR, _r);
+  analogWrite(outG, _g);
+  analogWrite(outB, _b);
+}
+
+void black() {
+  setColor(0, 0, 0);
 }
